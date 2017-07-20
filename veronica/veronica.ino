6dbc1b73d4b21ef6af0b939a5ec37cbfb307c2c7
Amber Donnelly <amberdonnelly@hotmail.ca>
2017-07-20 16:58:04 -0700

    first commit

---
+++ veronica/veronica.ino
@@ -0,0 +1,13 @@
+#include <phys253.h>
+#include <LiquidCrystal.h>
+#include <veronica.h>
+
+void setup() {
+  #include <phys253setup.txt>
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
